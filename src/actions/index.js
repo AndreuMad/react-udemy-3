5295865c6b4027cd3fa415c5e2323d25779801fe
Andrii Kit <andrii.labyk@gmail.com>
2017-06-27 22:08:28 +0300

    11

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '9234de4aecf7eaf69fde68432897eaf8';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
